4c0e743a4665a3fbabde9216118b67e02e5f2c00
luebbertevan <luebbertevan@gmail.com>
2025-09-30 11:56:16 -0400

    bare homepage

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,6 +1,3 @@
 import { createAuthClient } from "better-auth/react" // make sure to import from better-auth/react
 
-export const authClient = createAuthClient({
-    //you can pass client configuration here
-	baseURL: "http://localhost:3000"
-})
+export const authClient = createAuthClient({})
