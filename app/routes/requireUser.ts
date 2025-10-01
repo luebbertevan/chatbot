32fea5210af8c2fd6af9562482454c3c7d48f2f8
luebbertevan <luebbertevan@gmail.com>
2025-10-01 14:26:12 -0400

    protected requires auth

--- app/routes/requireUser.ts
+++ app/routes/requireUser.ts
@@ -1,9 +1,11 @@
 import { redirect, type ActionFunctionArgs, type LoaderFunctionArgs } from 'react-router'
 import { auth } from '~/auth.server' // Adjust the path as necessary
+import { authClient } from '~/lib/auth-client'
 
 
 export async function requireUser(request: LoaderFunctionArgs["request"]) {
   const session = await auth.api.getSession({ headers: request.headers });
+  console.log("SESSION: ", session)
   if (!session?.user) throw redirect("/signIn");
   return session.user;
 }
